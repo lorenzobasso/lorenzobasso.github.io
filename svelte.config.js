98b2802f609b56c9585a473f74d5b35a077e259d
lorenzobasso <lorenzobasso98@gmail.com>
2023-02-16 21:07:02 +0000

    Configure build

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,52 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
+
+import { mdsvex } from 'mdsvex'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeExternalLinks from 'rehype-external-links'
+import rehypeSlug from 'rehype-slug'
+import preprocess from 'svelte-preprocess'
+
+const extensions = ['.svelte', '.md']
+const dev = process.argv.includes('dev')
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
+		paths: { base: dev ? '' : '' },
+		appDir: 'internal',
 	},
 	preprocess: [
 		vitePreprocess({
 			postcss: true,
 		}),
+		preprocess({
+			postcss: true,
+			scss: true,
+			preserve: ['module'],
+		}),
+		mdsvex({
+			extensions,
+			rehypePlugins: [
+				rehypeExternalLinks,
+				rehypeSlug,
+				[
+					rehypeAutolinkHeadings,
+					{
+						behavior: 'append',
+						content: {
+							type: 'element',
+							tagName: 'span',
+							properties: { className: ['heading-link'] },
+							children: [{ type: 'text', value: '#' }],
+						},
+					},
+				],
+			],
+		}),
 	],
+	extensions,
 	vitePlugin: {
 		experimental: {
 			inspector: {
@@ -18,6 +54,7 @@ const config = {
 			},
 		},
 	},
+	target: '#svelte',
 }
 
 export default config
